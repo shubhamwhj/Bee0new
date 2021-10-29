507ca70002718b1da49a33e080991aaf32710263
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2021-10-29 16:05:31 +0530

    Update flappy0.py

--- flappy0.py
+++ flappy0.py
@@ -8,6 +8,19 @@ width=400
 height=600
 screen = pygame.display.set_mode((width,height))
   
+#Add code to create an empty dictionary "images"
+
+#add code to load the bird and the ground images in the dictionary("base.png", "redbird-midflap.png")
+
+#uncomment this part to load rest of the images.
+# images["bg1"] = pygame.image.load("bg1.png").convert_alpha()
+# images["pipe"] = pygame.image.load("pipe-red.png").convert_alpha()
+# images["message"] = pygame.image.load("message.png").convert_alpha()
+# images["over"] = pygame.image.load("gameover.png").convert_alpha()
+# images["cloud"]=pygame.image.load("cloud.png").convert_alpha()
+
+#create a rectangle for the bee.
+
 
 while True:    
     screen.fill((50,150,255))
@@ -17,7 +30,7 @@ while True:
             pygame.quit()
             sys.exit()
         
-    
+    #Add code to display bee and thr ground images.
    
     pygame.display.update()
     clock.tick(30)
